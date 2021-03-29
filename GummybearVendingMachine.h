45bcfa63f88432248b00c84f82ee53f3b6195578
Zsombor <kisszsombor@gmail.com>
2021-03-29 21:43:34 +0200

    setup in eclipse

---
+++ GummybearVendingMachine.h
@@ -0,0 +1,22 @@
+// Only modify this file to include
+// - function definitions (prototypes)
+// - include files
+// - extern variable definitions
+// In the appropriate section
+
+#ifndef _GummybearVendingMachine_H_
+#define _GummybearVendingMachine_H_
+#include "Arduino.h"
+//add your includes for the project GummybearVendingMachine here
+
+
+//end of add your includes here
+
+
+//add your function definitions for the project GummybearVendingMachine here
+
+
+
+
+//Do not add code below this line
+#endif /* _GummybearVendingMachine_H_ */
