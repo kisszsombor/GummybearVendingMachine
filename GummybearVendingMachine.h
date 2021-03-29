2dabefdbaa9c1b6d454678733f6cf3a6cf90115a
Zsombor <kisszsombor@gmail.com>
2021-03-29 22:33:12 +0200

    hd44780.h
    fix project to run with cpp code and eclipse ide
    added new include hd44780.h to handle I2C display, as the liquid cristal only printed the first character

--- GummybearVendingMachine.h
+++ GummybearVendingMachine.h
@@ -8,13 +8,44 @@
 #define _GummybearVendingMachine_H_
 #include "Arduino.h"
 //add your includes for the project GummybearVendingMachine here
-
+#include "Arduino.h"
+#include <Wire.h> // Wire Bibliothek einbinden
+#include <Keypad.h>
+#include <LiquidCrystal_I2C.h> // Vorher hinzugefugte LiquidCrystal_I2C Bibliothek einbinden
+#include <hd44780.h>
+#include <hd44780ioClass/hd44780_I2Cexp.h> // include i/o class header
 
 //end of add your includes here
 
 
 //add your function definitions for the project GummybearVendingMachine here
+void startScreen();
 
+void startGame(int selectedGame);
+
+void startRound(int selectedGame, int korSorszama);
+
+void checknumber(int n);
+
+void feladatScreen(int sorszam, int randNumber1, int randNumber2, int muvelet);
+
+void kiertekel(int megoldas, int tipp);
+
+void kiertekelJelszo();
+
+void stopGame();
+
+void resetGame();
+
+void resetInputNumbers();
+
+void openDoor();
+
+void jelszo();
+
+void toggleActiveGame();
+
+void debug(int nr);
 
 
 
